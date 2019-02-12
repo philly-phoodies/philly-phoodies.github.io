8520212d3c75aba7aa4e9aa1250f154554a1af4a
Katie <kmm692@drexel.edu>
2019-02-12 14:55:28 -0500

    updating baseline

--- searchBar.js
+++ searchBar.js
@@ -7,10 +7,17 @@ var enter_click = function () {
   var searchEntry = $("searchEntry").value;
   var price = $("price").value;
   var distance = $("distance").value;
+
+  const Http = new XMLHttpRequest();
+  const url='https://jsonplaceholder.typicode.com/posts';
+  Http.open("GET",url);
+  Http.send();
+  Http.onreadystatechange=(e)=>{console.log(Http.responseText)};
+
   alert (searchEntry);
   alert (price);
   alert (distance);
-  window.location="file:///Users/KatieMarie/Desktop/Drexel/201825/CI%20103/searchResults.html";
+  window.location="./searchResults.html";
 
   if ( searchEntry == Pizza || pizza) {
     window.location="https://www.google.com";
